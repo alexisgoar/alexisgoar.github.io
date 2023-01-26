90f26945017cc3ee648f7c92f23079158b3c21da
Alexis Gonzalez Argüello <alexis.gonzalez_argueello@ibeo-as.com>
2023-01-26 12:58:29 +0100

    Footer

---
+++ src/nav_script.js
@@ -0,0 +1,23 @@
+fetch('/elements/navigation.html')
+.then(res => res.text())
+.then(text => {
+    let oldelem = document.querySelector("script#replace_with_navbar");
+    let newelem = document.createElement("div");
+    newelem.innerHTML = text;
+    oldelem.parentNode.replaceChild(newelem,oldelem);
+}).then(() =>{
+    const hamburger = document.querySelector(".hamburger"); 
+    const menu = document.querySelector(".menu"); 
+
+    hamburger.addEventListener("click",() => {
+        hamburger.classList.toggle("active"); 
+        menu.classList.toggle("active"); 
+    })
+
+    document.querySelectorAll(".menu-item").forEach(n => n.addEventListener("click",()=>{
+        hamburger.classList.remove("active"); 
+        menu.classList.remove("active"); 
+    }))
+})
+
+
