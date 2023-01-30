05b7a4559258086cd9d6bba45ab0d27183e4d5d8
Alexis Gonzalez Argüello <alexis.gonzalez_argueello@ibeo-as.com>
2023-01-30 19:42:56 +0100

    restore

--- src/nav_script.js
+++ src/nav_script.js
@@ -1,4 +1,4 @@
-fetch('./elements/navigation.html')
+fetch('/elements/navigation.html')
 .then(res => res.text())
 .then(text => {
     let oldelem = document.querySelector("script#replace_with_navbar");
