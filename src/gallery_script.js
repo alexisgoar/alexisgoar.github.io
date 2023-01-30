1065490e2994a8ff1f51f7aa2342c9739d27819d
Alexis Gonzalez Argüello <alexis.gonzalez_argueello@ibeo-as.com>
2023-01-30 09:22:50 +0100

    Bugfixes, refinement and refactoring

---
+++ src/gallery_script.js
@@ -0,0 +1,9 @@
+fetch('/elements/gallery.html')
+.then(res => res.text())
+.then(text => {
+    let oldelem = document.querySelector("script#replace_with_gallery");
+    let newelem = document.createElement("div");
+    newelem.innerHTML = text;
+    oldelem.parentNode.replaceChild(newelem,oldelem);
+})
+
