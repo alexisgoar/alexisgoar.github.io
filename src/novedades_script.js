c8deeae11a825ccfcf73128287fdfe8e6f78df2b
Alexis Gonzalez Argüello <alexis.gonzalez_argueello@ibeo-as.com>
2023-01-31 11:33:32 +0100

    novedades page

---
+++ src/novedades_script.js
@@ -0,0 +1,9 @@
+fetch('/elements/novedades.html')
+.then(res => res.text())
+.then(text => {
+    let oldelem = document.querySelector("script#replace_with_novedades");
+    let newelem = document.createElement("div");
+    newelem.innerHTML = text;
+    oldelem.parentNode.replaceChild(newelem,oldelem);
+})
+
